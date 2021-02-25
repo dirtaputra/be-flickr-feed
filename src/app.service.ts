a4113f99154d91a78f98088a04f9f95a206e054b
Dirta Putra Anggara <dirtaputraanggara@gmail.com>
2021-02-25 14:15:56 +0700

    [ADD] create endpoint feed

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+import { toJson } from 'xml2json';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getFeed() {
+    const feedData: any = await axios.get(
+      'https://www.flickr.com/services/feeds/photos_public.gne',
+    );
+    const feed: any = toJson(feedData.data);
+    return feed;
   }
 }
