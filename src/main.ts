6f49e83a25af33b685822eb6cd5bcc88480e3a14
Dirta Putra Anggara <dirtaputraanggara@gmail.com>
2021-02-25 14:25:36 +0700

    [ADD] create swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger Config
+  const options = new DocumentBuilder()
+    .setTitle('flickr REST API')
+    .setVersion('1.0')
+    .addTag('flickr feed')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
